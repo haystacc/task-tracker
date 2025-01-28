95a5ec10fe7db06fb24f881882a26ec6012ddf65
Hayden Ho <haydenho2005@gmail.com>
2025-01-28 15:03:07 +1100

    created files to start working on task

---
+++ task-tracker.py
@@ -0,0 +1,8 @@
+import sys
+
+if sys.argv[1] == add: 
+    if len(sys.argv) != 3:
+        print("This command was not used correctly. Please see the manual for how to use this command correctly")
+    else: 
+        # add the task
+        print("Task added successfully.")
